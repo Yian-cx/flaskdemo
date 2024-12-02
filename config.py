a57adce9a95c5b572728717b413655ce4c2e4e20
Yian-cx <yiantime2002@gmail.com>
2024-12-02 22:08:51 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # 禁用 Flask-SQLAlchemy 的修改追踪
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:cxypc@localhost/bookdb'  # 使用正确的数据库连接 URI
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/bookdb'  # 使用正确的数据库连接 URI
